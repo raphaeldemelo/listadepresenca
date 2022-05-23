0b2ea2a9aacb9216bad65970ecefb60a62929e48
Raphael de Melo <raphaeldemelosoares@hotmail.com>
2022-05-23 12:38:01 -0300

    feat: criando página home

---
+++ src/Pages/Home/index.jsx
@@ -0,0 +1,11 @@
+import './styles.css'
+
+export default function Home () {
+  return (
+    <div className='container'>
+      <h1>Lista de presença</h1>
+      <input type='text' placeholder='Digite o nome...' />
+      <button type='button'>Adicionar</button>
+    </div>
+  )
+}
